17723982554f6dd39aa6c897cca6083f5349f19f
luis_enriquesm96@hotmail.com <luis_enriquesm96@hotmail.com>
2025-10-04 22:07:46 -0600

    first commit

---
+++ src/main/java/org/dsa/arrays/TwoNumSum.java
@@ -0,0 +1,24 @@
+package org.dsa.arrays;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoNumSum {
+    public static void main(String[] args) {
+    int[] result = twoNumberSum(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 15}, 18);
+    System.out.println(Arrays.toString(result));
+    }
+
+    public static int[] twoNumberSum(int[] array, int targetSum) {
+        Map<Integer, Integer> storedNumbers = new HashMap<>();
+        for(int i = 0; i < array.length; i++){
+            int sumUp = targetSum - array[i];
+            if(storedNumbers.containsKey(sumUp)){
+                return new int[] {sumUp, array[i]};
+            }
+            storedNumbers.put(array[i], i);
+        }
+        return new int[0];
+    }
+}
